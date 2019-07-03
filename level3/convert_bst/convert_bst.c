b5fb5c343087745d9f3b3ace0f0e02e678f95533
Chun chung michael Yuen <chyuen@e1z2r5p37.42.us.org>
2019-07-03 13:53:34 -0700

    bst

--- level3/convert_bst/convert_bst.c
+++ level3/convert_bst/convert_bst.c
@@ -21,27 +21,28 @@ struct s_node {
 struct s_node *convert_bst(struct s_node *bst)
 {
 	struct s_node *ret;
-	struct s_node *cur;
 
 	if (!bst)
 		return (NULL);
 	ret = convert_bst(bst->left);
-	if (!ret)
-		ret = bst;
-	else
-	{
-		cur = ret->left;
-		cur->right = bst;
-		bst->left = cur;
-	}
 	bst->right = convert_bst(bst->right);
+	if (ret)
+	{
+		ret->left->right = bst;
+		bst->left = ret->left;
+	}
+	else
+		ret = bst;
 	if (bst->right)
 	{
-		cur = bst->right->left;
+		bst->right->left->right = ret;
+		ret->left = bst->right->left;
 		bst->right->left = bst;
-		cur->right = ret;
 	}
 	else
+	{
 		bst->right = ret;
+		ret->left = bst;
+	}
 	return (ret);
-}	
+}
